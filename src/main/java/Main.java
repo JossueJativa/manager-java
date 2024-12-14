6dca5e5f6e4a4ba08459a0ed2ee0a5a3aef70ecf
Jossue <jativatamayojossuedavid@gmail.com>
2024-12-14 18:34:31 -0500

    Ingreso de codigo java

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        CManager m = new CManager();
+        m.aC("John Doe");
+        m.aC("Jane Smith");
+        m.aO("Order1");
+        m.aO("Order2");
+        m.pO("John Doe");
+        m.gR();
+    }
+}
