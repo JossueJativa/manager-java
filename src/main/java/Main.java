bbd9f611048d7f02f719bf45304dd1376cd992da
Jossue <jativatamayojossuedavid@gmail.com>
2024-12-14 23:00:27 -0500

    Se realiza el cambio de nombres para mejora de legibilidad

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        CManager m = new CManager();
-        m.aC("John Doe");
-        m.aC("Jane Smith");
-        m.aO("Order1");
-        m.aO("Order2");
-        m.pO("John Doe");
-        m.gR();
+        CustomerManager manageCustomer = new CustomerManager();
+        manageCustomer.addCustomer("John Doe");
+        manageCustomer.addCustomer("Jane Smith");
+        manageCustomer.addOrder("Order1");
+        manageCustomer.addOrder("Order2");
+        manageCustomer.processOrderByCustomer("John Doe");
+        manageCustomer.generateSummaryReport();
     }
 }
